5bf7259f5514a9a1ad07d165e33f5059d50d5836
Jack <jackbrewis96@gmail.com>
2019-06-06 12:20:09 -0400

    Allow user input for variable

--- mac_changer.py
+++ mac_changer.py
@@ -2,8 +2,8 @@
 
 import subprocess
 
-interface = "eth0"
-new_mac = "00:11:22:33:44:55"
+interface = input("interface > ")
+new_mac = input("new MAC >")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
