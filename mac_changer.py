8ea43426fadd6e40a1f3b94d6a7505cc17102857
Jack <jackbrewis96@gmail.com>
2019-06-06 13:40:06 -0400

    Handle missing command line arguments

--- mac_changer.py
+++ mac_changer.py
@@ -9,7 +9,13 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a MAC, use --help for more info")
+
+    return options
 
 
 def change_mac(interface, new_mac):
@@ -20,5 +26,5 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
