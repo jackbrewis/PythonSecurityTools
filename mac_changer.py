c7e9fcf70dcc7909379f0e30aa78224fffd22d87
Jack <jackbrewis96@gmail.com>
2019-06-06 12:31:17 -0400

    Safely handle user input

--- mac_changer.py
+++ mac_changer.py
@@ -8,6 +8,6 @@ new_mac = input("new MAC >")
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
