28939c8148bd0751207d4b0464d4aacc6c9c78b6
Jack <jackbrewis96@gmail.com>
2019-06-06 13:19:52 -0400

    Move logic to within a function

--- mac_changer.py
+++ mac_changer.py
@@ -3,6 +3,14 @@
 import subprocess
 import optparse
 
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
@@ -10,12 +18,5 @@ parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
 (options, arguments) = parser.parse_args()
 
-interface = options.interface
-new_mac = options.new_mac
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
-
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+change_mac(options.interface, options.new_mac)
