c25e7afd274113c79724391e16def252f0c5d1f0
Jack <jackbrewis96@gmail>
2019-06-06 11:37:02 -0400

    Update mac_changer to change mac address of eth0

--- mac_changer.py
+++ mac_changer.py
@@ -2,4 +2,6 @@
 
 import subprocess
 
-subprocess.call("ifconfig", shell=True)
+subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
+subprocess.call("ifconfig eth0 up", shell=True)
