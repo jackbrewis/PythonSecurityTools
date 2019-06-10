a87a0d7bd66b5712412bf7a5ad59e30fa8f83abd
Jack <jackbrewis96@gmail.com>
2019-06-10 13:03:58 -0400

    Broadcast arp and print responses

--- network_scanner.py
+++ network_scanner.py
@@ -5,8 +5,12 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
+    # dst is the standard broadcast MAC
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
+
+    # Use srp instead of sr as we have a custom Ether set for the packet
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
+
 scan("10.0.2.1/24")
