2da93a7cce2edd9f5c11da80d3b007d2f6437370
Jack <jackbrewis96@gmail.com>
2019-06-10 12:54:28 -0400

    Combine broadcast and arp requests

--- network_scanner.py
+++ network_scanner.py
@@ -4,7 +4,9 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
-
-
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    print(arp_request_broadcast.summary())
+    arp_request_broadcast.show()
 scan("10.0.2.1/24")
